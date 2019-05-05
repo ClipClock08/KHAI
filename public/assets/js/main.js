62e7b218d445b057921ca5222decab5e4a3fb268
clipclock08 <clipclock08@gmail.com>
2019-05-05 22:38:39 +0300

    responsive

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -30,5 +30,21 @@ function initMap() {
 
 $('.keynote__slider').slick({
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1199,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
